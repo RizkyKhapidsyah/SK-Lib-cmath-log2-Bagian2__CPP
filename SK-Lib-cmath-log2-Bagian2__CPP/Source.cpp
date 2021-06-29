d129bac7aa833e061995d78ce837e1aa8bd59f42
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-06-29 19:32:21 +0700

    Add project files.

---
+++ SK-Lib-cmath-log2-Bagian2__CPP/Source.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <conio.h>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	long b = 89;
+	float c = 5.3;
+	double d = 43.29;
+	long double e = 17.4;
+
+	cout << log2(b) << "\n";	
+	cout << log2(c) << "\n";
+	cout << log2(d) << "\n";
+	cout << log2(e) << "\n";
+
+	_getch();
+	return 0;
+}
